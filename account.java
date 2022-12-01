fa5fe28a348afc8e8d98f04f647593db405de59f
VIBINTP <118337078+VIBINTP@users.noreply.github.com>
2022-12-01 15:57:15 +0530

    Create account.java

---
+++ account.java
@@ -0,0 +1,12 @@
+public class account{
+
+    public static void main(String []args)
+    {
+    bank_details icici=new bank_details();
+    icici.bank_name="icici";
+    icici.intreast=7f;
+    System.out.println(icici.output());
+    }
+    
+    
+    }
